84cf4eb21c71de2c82c14426bbdbaa1ff8471cf8
MEHighKing <104882604+MEHighKing@users.noreply.github.com>
2023-02-21 14:42:22 +0530

    First Commit
    Adding all the script files

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Expecting you have installed both 'asciinema' and 'svg-term-cli'..."
+echo
+mkdir /home/$USER/svg-create-program
+mkdir /home/$USER/Pictures/terminal_SVG
+chmod +x recordterm
+chmod +x editshow
+chmod +x showing
+chmod +x svg-creator-help
+
+cp recordterm /home/$USER/svg-create-program
+cp editshow /home/$USER/svg-create-program
+cp showing /home/$USER/svg-create-program
+cp svg-creator-help /home/$USER/svg-create-program
+
+echo "export PATH=/home/$USER/svg-create-program:$PATH" >> ~/.bashrc
+echo "Thanks. It's Now installed. Restart current session to start working. Also, run svg-creator-help to get an overview"
+
